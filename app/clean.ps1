5bce474aa4eb0054223e89e75e67f16a91b9e753
Isaias Martinez <imartinez2@hotmail.com>
2022-10-31 15:21:44 -0400

    .

--- app/clean.ps1
+++ app/clean.ps1
@@ -1,22 +1,2 @@
-#Detener el container de la aplicación
-docker stop tema4lab1
-
-#Eliminar el container de la aplicación
-docker rm tema4lab1
-
-#Eliminar la imagen de la aplicación
-docker rmi tema4lab:v1.0
-
-#Detener el container de la base de datos
-docker stop mysqltodoapp
-
-#Eliminar el container de la base de datos
-docker rm mysqltodoapp
-
-#Eliminar la imagen de la base de datos
-docker rmi mysql:5.7
-
-#Eliminar la red de la aplicación
-docker network rm todo-app
-
-docker volume remove todo-mysql-data
+#Eliminar la aplicación
+docker compose down
--- app/clean.sh
+++ app/clean.sh
@@ -1,20 +1,2 @@
-#Detener el container de la aplicación
-docker stop tema4lab1
-
-#Eliminar el container de la aplicación
-docker rm tema4lab1
-
-#Eliminar la imagen de la aplicación
-docker rmi tema4lab:v1.0
-
-#Detener el container de la base de datos
-docker stop mysqltodoapp
-
-#Eliminar el container de la base de datos
-docker rm mysqltodoapp
-
-#Eliminar la imagen de la base de datos
-docker rmi mysql:5.7
-
-#Eliminar la red de la aplicación
-docker network rm todo-app
+#Eliminar la aplicación
+docker compose down
